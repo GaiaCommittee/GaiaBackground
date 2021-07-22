bf03202c6ef9ec03a8aebe9e93c4a315910732ff
Vincent <rumble.vecent@outlook.com>
2021-07-22 20:37:54 +0800

    Change the CMake module, make it easy to use, and add version support.

--- GaiaBackground/GaiaBackgroundConfig.cmake
+++
@@ -1,8 +0,0 @@
-cmake_minimum_required(3.10)
-
-find_path(GaiaBackground_INCLUDE_DIRS "GaiaBackground")
-find_library(GaiaBackground_LIBS "GaiaBackground")
-
-if (GaiaBackground_INCLUDE_DIRS AND GaiaBackground_LIBS)
-    set(GaiaBackground_FOUND TRUE)
-endif()
